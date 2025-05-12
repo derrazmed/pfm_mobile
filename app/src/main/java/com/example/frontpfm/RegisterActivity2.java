7fa8694fbad5d42f07bd08c1c4429d802f00907e
derrazmed <derrmohamed3@gmail.com>
2025-05-12 22:40:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/frontpfm/RegisterActivity2.java
@@ -0,0 +1,11 @@
+package com.example.frontpfm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class RegisterActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register2);
+    }
+}
